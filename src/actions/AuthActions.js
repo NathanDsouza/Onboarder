74cfe0c3b41e0a9953e6866d1bd05378e0fa3c66
Nathan <nathan.m.dsouza@gmail.com>
2019-04-07 02:06:25 -0400

    eslint stuff

--- src/actions/AuthActions.js
+++ src/actions/AuthActions.js
@@ -1,8 +1,8 @@
-import firebase from "@firebase/app";
-import "@firebase/database";
-import "@firebase/auth";
-import NavigationService from "../actions/NavigationService";
-import { setUserData } from "./FirebaseService";
+import firebase from '@firebase/app';
+import '@firebase/database';
+import '@firebase/auth';
+import NavigationService from '../actions/NavigationService';
+import { setUserData } from './FirebaseService';
 
 import {
   EMAIL_CHANGED,
@@ -12,67 +12,25 @@ import {
   LOGIN_USER_FAIL,
   LOGIN_USER,
   LOGOUT_USER,
-  UPDATE_PROFILE
-} from "./types";
+  UPDATE_PROFILE,
+} from './types';
 
-export const emailChanged = text => {
-  return {
-    type: EMAIL_CHANGED,
-    payload: text
-  };
-};
+export const emailChanged = text => ({
+  type: EMAIL_CHANGED,
+  payload: text,
+});
 
-export const passwordChanged = text => {
-  return {
-    type: PASSWORD_CHANGED,
-    payload: text
-  };
-};
-
-export const loginUser = ({ email, password }) => {
-  return dispatch => {
-    dispatch({ type: LOGIN_USER });
-
-    firebase
-      .auth()
-      .signInWithEmailAndPassword(email, password)
-      .then(user => {
-        setUserData(dispatch);
-        loginUserSuccess(dispatch, user);
-      })
-      .catch(error => {
-        console.log(error);
-        firebase
-          .auth()
-          .createUserWithEmailAndPassword(email, password)
-          .then(user => {
-            const { currentUser } = firebase.auth();
-            firebase
-              .database()
-              .ref(`/users/${currentUser.uid}`)
-              .set({ email })
-              .then(() => {
-                newUserSuccess(dispatch, user, email);
-              })
-              .catch(error => {
-                console.log(error);
-                loginUserFail(dispatch, error);
-              });
-          })
-          .catch(error => {
-            console.log(error);
-            loginUserFail(dispatch, error);
-          });
-      });
-  };
-};
+export const passwordChanged = text => ({
+  type: PASSWORD_CHANGED,
+  payload: text,
+});
 
 const loginUserSuccess = (dispatch, user) => {
   dispatch({
     type: LOGIN_USER_SUCCESS,
-    payload: user
+    payload: user,
   });
-  NavigationService.resetNavigation("Welcome");
+  NavigationService.resetNavigation('Welcome');
 };
 
 const newUserSuccess = (dispatch, user, email) => {
@@ -83,33 +41,67 @@ const newUserSuccess = (dispatch, user, email) => {
   dispatch({
     type: UPDATE_PROFILE,
     email,
-  })
-  NavigationService.resetNavigation("Profile");
+  });
+  NavigationService.resetNavigation('Profile');
 };
 
 const loginUserFail = (dispatch, error) => {
   dispatch({
     type: LOGIN_USER_FAIL,
-    payload: error.toString()
+    payload: error.toString(),
   });
-};
-
-export const logoutUser = () => {
-  return dispatch => {
-    firebase
-      .auth()
-      .signOut()
-      .then(user => logoutUserSuccess(dispatch, user))
-      .catch(error => {
-        console.log(error);
-      });
-  };
 };
 
 const logoutUserSuccess = (dispatch, user) => {
   dispatch({
     type: LOGOUT_USER,
-    payload: user
+    payload: user,
   });
-  NavigationService.resetNavigation("LandingScreen");
+  NavigationService.resetNavigation('LandingScreen');
+};
+
+export const logoutUser = () => (dispatch) => {
+  firebase
+    .auth()
+    .signOut()
+    .then(user => logoutUserSuccess(dispatch, user))
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+export const loginUser = ({ email, password }) => (dispatch) => {
+  dispatch({ type: LOGIN_USER });
+
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(email, password)
+    .then((user) => {
+      setUserData(dispatch);
+      loginUserSuccess(dispatch, user);
+    })
+    .catch((error) => {
+      console.log(error);
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(email, password)
+        .then((user) => {
+          const { currentUser } = firebase.auth();
+          firebase
+            .database()
+            .ref(`/users/${currentUser.uid}`)
+            .set({ email })
+            .then(() => {
+              newUserSuccess(dispatch, user, email);
+            })
+            .catch((error) => {
+              console.log(error);
+              loginUserFail(dispatch, error);
+            });
+        })
+        .catch((error) => {
+          console.log(error);
+          loginUserFail(dispatch, error);
+        });
+    });
 };
